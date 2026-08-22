d031b1a452fa0657e9a54ab2bea8b4fae3235274
GOWTHAM-MG-2006 <mggowtham2006@gmail.com>
2026-08-22 07:28:24 +0530

    feat: Flutter APK v2.3.1 - new Razorpay live key + release signing keystore

--- escq_canteen/lib/config.dart
+++ escq_canteen/lib/config.dart
@@ -2,5 +2,5 @@ class AppConfig {
   static const String apiBase = 'https://canteen20.vercel.app';
   static const String appName = 'Esc(Q)';
   static const String appVersion = '2.3.0';
-  static const String razorpayKeyId = 'rzp_live_TOtfO2Qm3tZYjn';
+  static const String razorpayKeyId = 'rzp_live_TSdI5TtF4Yo2vq';
 }
